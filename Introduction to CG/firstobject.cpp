dd7442e21ed50918b5d0060d43b249c34963dd93
clydepp <pangiclyde@gmail.com>
2024-12-18 01:38:44 +0000

    Changes to the logic of x_proj and y_proj.

--- Introduction to CG/firstobject.cpp
+++ Introduction to CG/firstobject.cpp
@@ -4,21 +4,21 @@ typedef float Point[3];
 
 int main(){
     Point corners[8] = {
-        {0, 0, 0},
-        {12, 0, 0},
-        {12, 8, 0},
-        {0, 8, 0},
-        {0, 0, 10},
-        {12, 0, 10},
-        {12, 8, 10},
-        {0, 8, 10},
+        { 1, -1, -5},
+        { 1, -1, -3},
+        { 1,  1, -5},
+        { 1,  1, -3},
+        {-1, -1, -5},
+        {-1, -1, -3},
+        {-1,  1, -5},
+        {-1,  1, -3}
     };
 
     const unsigned int image_width = 512, image_height = 512;
 
     for (int i = 0; i < 8; ++i){
-        float x_proj = corners[i][0] / corners[i][2]; // P'.x
-        float y_proj = corners[i][1] / corners[i][2]; // P'.y
+        float x_proj = corners[i][0] / -corners[i][2]; // -P'.x
+        float y_proj = corners[i][1] / -corners[i][2]; // -P'.y
 
         float x_proj_remap = (1 + x_proj) / 2;
         float y_proj_remap = (1 + y_proj) / 2;
